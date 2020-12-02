5bd082f5fcc3c7417972aa71bc6269bb8c39df5d
Jean Senecal <jeansenecal02@gmail.com>
2020-12-01 19:33:30 -0500

    Start git repo

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image : '/images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: "high quality shirt",
+        },
+        {
+            _id: '2',
+            name: 'Adidas Fit Shirt',
+            category: 'Shirt',
+            image : '/images/p2.jpg',
+            price: 67,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: "high quality shirt",
+        },
+        {
+            _id: '3',
+            name: 'Lacoste Slim Shirt',
+            category: 'Shirts',
+            image : '/images/p3.jpg',
+            price: 34,
+            brand: 'Lacoste',
+            rating: 5,
+            numReviews: 100,
+            description: "high quality shirt",
+        },
+        {
+            _id: '4',
+            name: 'Nike Slim Pants',
+            category: 'Pants',
+            image : '/images/p4.jpg',
+            price: 41,
+            brand: 'Nike',
+            rating: 1.5,
+            numReviews: 1,
+            description: "high quality pants",
+        },
+        {
+            _id: '5',
+            name: 'Adidas Slim Pants',
+            category: 'Pants',
+            image : '/images/p5.jpg',
+            price: 46,
+            brand: 'Adidas',
+            rating: 2,
+            numReviews: 14,
+            description: "high quality pants",
+        },
+        {
+            _id: '6',
+            name: 'Lacoste Slim Shirt',
+            category: 'Pants',
+            image : '/images/p6.jpg',
+            price: 76,
+            brand: 'Lacoste',
+            rating: 3.5,
+            numReviews: 20,
+            description: "high quality pants",
+        }
+
+    ]
+}
+export default data;
