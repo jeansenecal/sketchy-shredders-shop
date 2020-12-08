c3562da128a7f9fe31b5ccdeb563a09a0366227b
Jean Senecal <jeansenecal02@gmail.com>
2020-12-07 20:36:47 -0500

    Start of backend

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import data from "./data.js";
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
+app.get("/api/products", (req,res) => {
+    res.send(data.products);
+})
+app.get('/', (req, res) => {
+    res.send("server is running");
+})
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+    console.log("Serve at http://localhost:" + port);
+})
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import data from '../data';
+import React, { useEffect, useState } from 'react';
 import Product from "../components/Product";
+import axios from 'axios';
+import MessageBox from '../components/MessageBox';
+import LoadingBox from '../components/LoadingBox';
 
 export default function HomeScreen() {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setLoading(true);
+                const {data} = await axios.get('http://localhost:5000/api/products');
+                setLoading(false);
+                setProducts(data);
+            } catch (err){
+                setError(err.message);
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, [])
     return (
-        <div className="row center">
+        <div>
             {
-                data.products.map(product => (
-                    <Product key={product.id} product={product} />
-                ))
+                loading ?( <LoadingBox/>)
+                :
+                error? (<MessageBox variant="danger">{error}</MessageBox>)
+                :
+                (
+                    <div className="row center">
+                        {
+                            products.map(product => (
+                                <Product key={product.id} product={product} />
+                            ))
+                        }
+                    </div>
+                )
             }
         </div>
     )
